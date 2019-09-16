11f163d6246ea796c0f456b1e8207f52d006eebd
haobingfu <haobingfu@frogsing.com>
2019-09-16 13:45:39 +0800

    commit

--- server/front/src/main/java/com/frogsing/front/web/hy/MemberAction.java
+++ server/front/src/main/java/com/frogsing/front/web/hy/MemberAction.java
@@ -8,12 +8,10 @@ import com.frogsing.heart.web.BaseAction;
 import com.frogsing.heart.web.Msg;
 import com.frogsing.heart.web.Result;
 import com.frogsing.heart.web.login.ILoginUser;
-import com.frogsing.member.po.*;
+import com.frogsing.member.po.Member;
 import com.frogsing.member.service.AuthapplyService;
 import com.frogsing.member.service.MemberService;
 import com.frogsing.member.service.UserService;
-import com.frogsing.member.utils.MEMBER.AuthenticateType;
-import com.frogsing.member.utils.MEMBER.ImageType;
 import com.frogsing.member.utils.MEMBER.MemberType;
 import com.frogsing.member.vo.LoginUser;
 import com.frogsing.member.vo.MemVo;
@@ -25,7 +23,10 @@ import org.apache.shiro.authz.annotation.RequiresRoles;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.multipart.commons.CommonsMultipartFile;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
@@ -140,6 +141,25 @@ public class MemberAction extends BaseAction {
             e.printStackTrace();
         }
         return authApply(500,model, request);
+    }
+
+    @RequestMapping(value = "authapply.json")
+    @ResponseBody
+    public Result Authapply(MemVo memVo,
+                              Model model, RedirectAttributes rmodel, ServletRequest request) {
+        try {
+            ILoginUser user = ShiroUtils.getCurrentUser();
+
+            memberService.doAuthApply_b(memVo,user);
+
+            return Result.success();
+        } catch (ServiceException e) {
+            e.printStackTrace();
+            return Result.failure(e.getMessage());
+        } catch (Exception e) {
+            e.printStackTrace();
+            return Result.failure("系统错误，请联系管理员");
+        }
     }
 
     @RequestMapping(value = "uploadimg.html")
