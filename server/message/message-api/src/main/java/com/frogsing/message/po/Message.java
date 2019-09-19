f62954592b2c36b59e690a6f58fe0040ec7e7dbe
haobingfu <haobingfu@frogsing.com>
2019-09-19 15:55:29 +0800

    commit

--- server/message/message-api/src/main/java/com/frogsing/message/po/Message.java
+++ server/message/message-api/src/main/java/com/frogsing/message/po/Message.java
@@ -2,8 +2,13 @@ package com.frogsing.message.po;
 //frogsingcode//
 
 
-import javax.persistence.Entity;
-import javax.persistence.Table;
+import com.frogsing.message.utils.MESSAGECol;
+import com.frogsing.operator.utils.OPERATORCol;
+import com.google.common.collect.Lists;
+
+import javax.persistence.*;
+import java.util.List;
+
 //frogsingcode//
 @Entity
 @Table(name = "cx_message")
@@ -14,10 +19,22 @@ public class Message  extends  MessageBase{
 	private static final long serialVersionUID = 1L;
 	
 //frogsingcode//
-	
-	
-	
-	
+
+
+	private List<MessageDetail> messageDetails = Lists.newArrayList();
+
+	@OneToMany(fetch= FetchType.LAZY)
+	@JoinColumn(name = MESSAGECol.cx_messagedetail.smessageid,updatable = false, insertable = false, nullable = true)
+	@OrderBy("dsenddatetime asc ")
+
+	public List<MessageDetail> getMessageDetails() {
+		return messageDetails;
+	}
+
+	public void setMessageDetails(List<MessageDetail> messageDetails) {
+		this.messageDetails = messageDetails;
+	}
+
 	//frogsingcode//
 	
 }
